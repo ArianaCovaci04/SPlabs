d52a2247347bd8eeada3922626f27ecf6a9dc8bb
ArianaCovaci04 <115119283+ArianaCovaci04@users.noreply.github.com>
2022-10-19 12:13:24 +0300

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,9 @@
+public class Author {
+    private final String nume;
+    public Book(String nume) {
+        this.nume = nume;
+    }
+    public void print() {
+        System.out.println(nume);
+    }
+}
