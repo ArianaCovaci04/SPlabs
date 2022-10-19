f56c14cf3dd92f5fbf17146320f8e64e8bce0a43
ArianaCovaci04 <115119283+ArianaCovaci04@users.noreply.github.com>
2022-10-19 12:18:01 +0300

    Delete Author.java

--- Author.java
+++
@@ -1,9 +0,0 @@
-public class Author {
-    private final String nume;
-    public Book(String nume) {
-        this.nume = nume;
-    }
-    public void print() {
-        System.out.println(nume);
-    }
-}
